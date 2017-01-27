b2f57ac5c557ce1626ae80b75648911117824404
Kevin Tong <ktong@ucvts.tec.nj.us>
2017-01-27 06:39:51 +0000

    added new scatter charts

---
+++ scatter_naturalgas.py
@@ -0,0 +1,21 @@
+# import modules
+from bokeh.charts import Scatter, output_file, save
+import numpy as np
+import pandas as pd
+import csv
+
+# get data into dataframe
+df = pd.read_csv('energydata.csv')
+
+# sort by year/month
+df.sort_values(by = 'Year/Month')
+
+# plot graph
+plot_1 = Scatter(df, x = 'Year/Month', y = 'Natural Gas Energy (kJ)', color = 'Year', title = "Natural Gas Energy by Month and Year",
+            xlabel = "Year-Month", ylabel = "Energy Produced (kJ)")
+
+# output file
+output_file("energychart_scatter_naturalgas.html")
+
+# save
+save(plot_1)
